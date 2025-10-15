1c2005f9af2c51c88193da2dac30f964d324af5c
nimo-mohamed <138133949+nimo-mohamed@users.noreply.github.com>
2025-10-15 13:25:04 +0100

    fix onPageLoad continuation and add button

--- app/controllers/EstimateIncomeTaxController.scala
+++ app/controllers/EstimateIncomeTaxController.scala
@@ -10,13 +10,11 @@ import views.html.EstimateIncomeTaxView
 class EstimateIncomeTaxController @Inject()(
                                        override val messagesApi: MessagesApi,
                                        identify: IdentifierAction,
-                                       getData: DataRetrievalAction,
-                                       requireData: DataRequiredAction,
                                        val controllerComponents: MessagesControllerComponents,
                                        view: EstimateIncomeTaxView
                                      ) extends FrontendBaseController with I18nSupport {
 
-  def onPageLoad: Action[AnyContent] = (identify andThen getData andThen requireData) {
+  def onPageLoad: Action[AnyContent] = (identify) {
     implicit request =>
       Ok(view())
   }
